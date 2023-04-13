788d62cde6950a686d2290c5ac34d5893d694e70
Julian <jrg180004@utdallas.edu>
2023-04-13 15:29:19 -0500

    Fixed errors and upgraded version

--- Ruddy Duck/Assets/Scripts/Units/BaseUnit.cs
+++ Ruddy Duck/Assets/Scripts/Units/BaseUnit.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class BaseUnit : MonoBehaviour
 {
+    string unitName = "";
     public double maxHP = 10;
     public double currentHP { get; private set; }
     public double Attack;
     public double Defense;
     public double Speed;
 
-    public public HUDHealth HPBar;
+    // public HUDHealth HPBar;
 }
