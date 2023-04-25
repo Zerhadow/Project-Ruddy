adb9606e7e1bf15b7a0732ff27f04a4d35adb858
Youssef <youssef.donia@gmail.com>
2023-04-25 12:14:16 -0500

    added more script connections

--- Ruddy Duck/Assets/Scripts/UI/DisplayStatsOfParty.cs
+++ Ruddy Duck/Assets/Scripts/UI/DisplayStatsOfParty.cs
@@ -80,16 +80,41 @@ public class DisplayStatsOfParty : MonoBehaviour
     void DisplayStats(BaseUnit player) {
         // Update text fields with player stats and display
         nameText.text = player.unitName;
-        Debug.Log("Player object: " + player.unitName);
-        // hpText.text = "HP: " + player.currentHP + "/" + player.maxHP;
+        // Debug.Log("Player object: " + player.unitName);
+        hpText.text = "HP: " + player.currentHP + "/" + player.maxHP;
         // Debug.Log("Player HP: " + player.currentHP + "/" + player.maxHP);
-        // attackText.text = "Attack: " + player.Attack;
+        attackText.text = "Atk: " + player.Attack;
         // Debug.Log("Player attack: " + player.Attack);
-        // defenseText.text = "Defense: " + player.Defense;
+        defenseText.text = "Def: " + player.Defense;
         // Debug.Log("Player defense: " + player.Defense);
-        // speedText.text = "Speed: " + player.Speed;
+        speedText.text = "Spd: " + player.Speed;
         // Debug.Log("Player speed: " + player.Speed);
-        // skillText.text = "Skill: " + player.skill;
+        skillText.text = "Skl: " + player.skill;
         // Debug.Log("Player skill: " + player.skill);
+
+        // UNIT 1
+        // nameText1.text = unit1.unitName;
+        // hpText1.text = "HP: " + unit1.currentHP + "/" + unit1.maxHP;
+        // attackText1.text = "Atk: " + unit1.Attack;
+        // defenseText1.text = "Def: " + unit1.Defense;
+        // speedText1.text = "Spd: " + unit1.Speed;
+        // skillText1.text = "Skl: " + unit1.skill;
+
+        // // UNIT 2
+        // nameText2.text = unit2.unitName;
+        // hpText2.text = "HP: " + unit2.currentHP + "/" + unit2.maxHP;
+        // attackText2.text = "Atk: " + unit2.Attack;
+        // defenseText2.text = "Def: " + unit2.Defense;
+        // speedText2.text = "Spd: " + unit2.Speed;
+        // skillText2.text = "Skl: " + unit2.skill;
+
+        // // UNIT 3
+        // nameText3.text = unit3.unitName;
+        // hpText3.text = "HP: " + unit3.currentHP + "/" + unit3.maxHP;
+        // attackText3.text = "Atk: " + unit3.Attack;
+        // defenseText3.text = "Def: " + unit3.Defense;
+        // speedText3.text = "Spd: " + unit3.Speed;
+        // skillText3.text = "Skl: " + unit3.skill;
+
     }
 }
