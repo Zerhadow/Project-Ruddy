8997c2e654edd37009661fb72aae28201d3e0154
Julian <jrg180004@utdallas.edu>
2023-04-27 13:17:29 -0500

    Player can leave scene after entering zone
    Working on colliders with NPC

--- Ruddy Duck/Assets/Scripts/InteractWithNPC.cs
+++ Ruddy Duck/Assets/Scripts/InteractWithNPC.cs
@@ -6,35 +6,36 @@ public class InteractWithNPC : MonoBehaviour
 {
     public float interactDistance = 2f;
     public GameObject unitsPannelObj;
+    public GameObject textBoxObj;
     KeyCode interactKey = KeyCode.E;
-    bool interacted = false;
+    bool npcInteract = false;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        textBoxObj.SetActive(false);
     }
 
-    // Update is called once per frame
     void FixedUpdate() {
-        if(Input.GetKey(interactKey)) {
+        if(Input.GetKey(interactKey) && npcInteract) {
             Debug.Log("Interact key pressed");
 
-            if(interacted) {
-                Debug.Log("Interacted with NPC");
-                unitsPannelObj.SetActive(true);
-            }
+            textBoxObj.SetActive(false);
+            unitsPannelObj.SetActive(true);
         }
     }
 
     private void OnTriggerEnter(Collider other) {
         Debug.Log("Player in range for interaction");
-        interacted = true;
+        npcInteract = true;
+
+        textBoxObj.SetActive(true);
     }
 
     private void OnTriggerExit(Collider other) {
         Debug.Log("Player out of range for interaction");
-        interacted = false;
+        npcInteract = false;
+
+        textBoxObj.SetActive(false);
     }
 
     private void OnDrawGizmos() {
