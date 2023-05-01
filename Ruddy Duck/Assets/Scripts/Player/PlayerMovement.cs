cc0ff98afb0f79635abf57ad055309ae0d37be97
Julian <jrg180004@utdallas.edu>
2023-05-01 12:25:56 -0500

    player animations work

--- Ruddy Duck/Assets/Scripts/Player/PlayerMovement.cs
+++ Ruddy Duck/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,22 +7,27 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 3f;
     public float rotationSpeed = 90f;
+    Animator anim;
 
     CharacterController characterController;
 
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
-    void Update() {
+    void FixedUpdate() {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        Vector3 direction = transform.forward * vertical + transform.right * horizontal;
+        anim.SetFloat("Velocity", direction.magnitude);
+        // Debug.Log(direction.magnitude);
 
-        if (direction.magnitude >= 0.1f) {
+        if (direction != Vector3.zero) {
             characterController.SimpleMove(direction * speed);
+            // Debug.Log("Moving: " + direction * speed);
 
             Quaternion targetRotation = Quaternion.LookRotation(direction);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
