e4001bd2ecdccc99f1e203c59982fbb6d0d99506
Julian <jrg180004@utdallas.edu>
2023-05-01 12:26:31 -0500

    Revert "Merge branch 'audio' into main"
    This reverts commit 9c01561b73f43d48fe7b7b741ae25fa1971f8e4c.

--- Ruddy Duck/Assets/Scripts/Player/PlayerController.cs
+++ Ruddy Duck/Assets/Scripts/Player/PlayerController.cs
@@ -6,45 +6,15 @@ public class PlayerController : MonoBehaviour
 {
     public int gold = 100;
 
-    public AudioSource audioSource;
-    public AudioClip quackSound;
-    public AudioClip eatingSound;
-
-    private bool isColliding = false;
-
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        // play quack sound effect when Q button is pressed 
-        if (Input.GetKeyDown(KeyCode.Q)) {
-            audioSource.PlayOneShot(quackSound);
-        }
-
-        // play eating sound effect if player collided with object and presses E
-        //  && Input.GetKeyDown(KeyCode.E)
-        if (isColliding) {
-            audioSource.PlayOneShot(eatingSound);
-        }
-
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Food") {
-            isColliding = true;
-        }
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.tag == "Food") {
-            isColliding = false;
-        }
+        
     }
 }
