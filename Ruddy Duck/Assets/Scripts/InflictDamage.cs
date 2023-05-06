5fdd02c14ab3dde602e63132fab3c138ea556c05
Julian <jrg180004@utdallas.edu>
2023-05-06 14:10:46 -0500

    Enemy takes dmg and formulas work

--- Ruddy Duck/Assets/Scripts/InflictDamage.cs
+++ Ruddy Duck/Assets/Scripts/InflictDamage.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class InflictDamage : MonoBehaviour
 {
+    private BaseUnit unitStats;
+
+    void Awake() {
+        unitStats = GetComponent<BaseUnit>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +22,12 @@ public class InflictDamage : MonoBehaviour
         
     }
 
-    private void OnTrigger(Collider other) {
+    private void OnTriggerEnter(Collider other) {
         if(other.tag == "Enemy") {
             Debug.Log("Sword Hit: " + other.name);
             // other.GetComponent<EnemyHealth>().TakeDamage(10);
+            BaseUnit enemyStats = other.GetComponent<BaseUnit>();
+            other.GetComponent<BaseUnit>().TakeDamage(unitStats, enemyStats);
         }
     }
 }
